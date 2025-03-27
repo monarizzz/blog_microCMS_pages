d9751c4bd3415739120aab67c423bace32648165
Cloud <monaniza@gmail.com>
2025-03-27 20:30:45 +0900

    HomePageMainに型をつける

--- src/features/blog/home/HomePageMain/HomePageMain.tsx
+++ src/features/blog/home/HomePageMain/HomePageMain.tsx
@@ -1,17 +1,21 @@
 import BlogCard from "@/commons/blog/BlogCard/BlogCard";
 import styles from "./HomePageMain.module.css";
 import Commonlayout from "@/commons/layout/Layout/CommonLayout";
+import { NextPage } from "next";
+import { Blog } from "@/infra/microCMS/schema/blog";
 
 type Props = {
-  blog: any;
+  blog: Blog[];
 };
 
-const HomeMain: React.FC<Props> = ({ blog }) => {
+const HomeMain: NextPage<Props> = ({ blog }) => {
   return (
     <Commonlayout>
       <div className={styles.container}>
         <div className={styles.title}>ホーム</div>
-        {blog.map((blog) => BlogCard({ blog }))}
+        {blog.map((blog) => (
+          <BlogCard blog={blog} />
+        ))}
       </div>
     </Commonlayout>
   );
