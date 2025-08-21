437f0affe43ccb925e38f44a220d4c70d785ace0
DESKTOP-windows11 <monariza915@gmail.com>
2025-08-21 16:54:37 +0900

    変更：引数をblogwithPlainTextに変更

--- src/commons/blog/BlogCard/BlogCard.tsx
+++ src/commons/blog/BlogCard/BlogCard.tsx
@@ -2,33 +2,26 @@ import Link from "next/link";
 import styles from "./BlogCard.module.css";
 import RelativeDate from "../../date/RelativeDate/RelativeDate";
 import "dayjs/locale/ja";
-import { Blog } from "@/infra/microCMS/schema/Blog/blog";
 import { NextPage } from "next";
 import { CategoryList } from "@/infra/microCMS/schema/Category/categoryList";
-import { useEffect, useState } from "react";
+import { BlogWithPlainText } from "@/infra/microCMS/schema/Blog/blogWithPlainText";
 
 type Props = {
-  blog: Blog;
+  blogsWithPlainText: BlogWithPlainText;
   category: CategoryList;
 };
 
-const BlogCard: NextPage<Props> = ({ blog, category }) => {
-  const publishedDate = new Date(blog.publishedAt);
-  const [html, setHtml] = useState("");
-
-  //　Hydration Failed を回避し、クライアントのみでHTMLを生成するため
-  useEffect(() => {
-    setHtml(blog.body);
-  }, [blog.body]);
+const BlogCard: NextPage<Props> = ({ blogsWithPlainText, category }) => {
+  const publishedDate = new Date(blogsWithPlainText.publishedAt);
 
   return (
     <div className={styles.card}>
-      <Link href={`/article/${blog.id}`}>
-        <div className={styles.title}>{blog.title}</div>
+      <Link href={`/article/${blogsWithPlainText.id}`}>
+        <div className={styles.title}>{blogsWithPlainText.title}</div>
         <div className={styles.date}>{RelativeDate(publishedDate)} </div>
         <>
           {category.map((tag) =>
-            blog.categories.map((articleCategory) =>
+            blogsWithPlainText.categories.map((articleCategory) =>
               tag.id == articleCategory.id ? (
                 <span className={styles.tag} key={tag.id}>
                   {tag.name}
@@ -37,13 +30,9 @@ const BlogCard: NextPage<Props> = ({ blog, category }) => {
             )
           )}
         </>
-
-        <div
-          className={styles.bodyText}
-          dangerouslySetInnerHTML={{
-            __html: html,
-          }}
-        />
+        <div className={styles.bodyText}>
+          {blogsWithPlainText.plainTextBody}
+        </div>
       </Link>
     </div>
   );
