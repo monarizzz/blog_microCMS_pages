86703cb8844cd7ab5df1edcf46c17ebc12c55fb4
Cloud <monaniza@gmail.com>
2025-08-29 14:44:20 +0900

    変更：更新アイコンをCSSからsvgへ

--- src/features/blog/article/components/ArticlePageMain/ArticlePageMain.tsx
+++ src/features/blog/article/components/ArticlePageMain/ArticlePageMain.tsx
@@ -28,7 +28,22 @@ const ArticlePageMain: NextPage<Props> = ({ blog, category, prevPost, nextPost }
             </div>
             {blog.publishedAt == blog.revisedAt ? null : (
               <>
-                <span className={styles.updateIcon}></span>
+                                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className={styles.updateIcon}>
+                  <path d="M3 2v6h6" />
+                  <path d="M21 12A9 9 0 0 0 6 5.3L3 8" />
+                  <path d="M21 22v-6h-6" />
+                  <path d="M3 12a9 9 0 0 0 15 6.7l3-2.7" />
+                </svg>
                 {dayjs(blog.revisedAt).format("YYYY/MM/DD HH:mm")}
               </>
             )}
