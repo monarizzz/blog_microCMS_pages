39faacfbdcf25f72b29ac514656f8bd37cba64cb
Cloud <monaniza@gmail.com>
2025-09-17 16:36:41 +0900

    fix: 大文字小文字で衝突したので一旦削除

--- src/infra/microCMS/schema/Blog/ArticleNavigation.ts
+++
@@ -1,8 +0,0 @@
-import { Blog } from "@/infra/microCMS/schema/Blog/blog";
-
-export type AdjacentArticle = Pick<Blog, "id" | "title"> | null;
-
-export type ArticleNavigation = {
-  prevArticle: AdjacentArticle;
-  nextArticle: AdjacentArticle;
-};
