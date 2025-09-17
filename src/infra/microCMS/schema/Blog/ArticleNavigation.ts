2bff616fc716fdb6ae4542a80ee2b0cf30c36edb
Cloud <monaniza@gmail.com>
2025-09-17 14:58:38 +0900

    fix: 型定義が小文字だった

--- src/infra/microCMS/schema/Blog/ArticleNavigation.ts
+++ src/infra/microCMS/schema/Blog/ArticleNavigation.ts
@@ -1,8 +1,8 @@
 import { Blog } from "@/infra/microCMS/schema/Blog/blog";
 
-export type adjacentArticle = Pick<Blog, "id" | "title"> | null;
+export type AdjacentArticle = Pick<Blog, "id" | "title"> | null;
 
-export type articleNavigation = {
-  prevArticle: adjacentArticle;
-  nextArticle: adjacentArticle;
+export type ArticleNavigation = {
+  prevArticle: AdjacentArticle;
+  nextArticle: AdjacentArticle;
 };
