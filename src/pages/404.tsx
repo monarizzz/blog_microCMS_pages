e7437473e2febd7a1d31b249e1c5e6d1e63b3101
Cloud <monaniza@gmail.com>
2025-09-30 19:28:02 +0900

    feat: ヘッダーを読み込み

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,12 @@
+import Commonlayout from "@/commons/layout/Layout/CommonLayout";
+
 const Custom404 = () => {
   return (
-    <main className="main">
-      <p>ページがありません。</p>
-    </main>
+    <Commonlayout>
+      <main className="main">
+        <p>ページがありません。</p>
+      </main>
+    </Commonlayout>
   );
 };
 export default Custom404;
