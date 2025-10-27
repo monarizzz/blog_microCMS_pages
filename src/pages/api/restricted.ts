451e16d35857893760db2c32c3f7cbaf1af5b612
Cloud <monaniza@gmail.com>
2025-10-27 19:31:24 +0900

    fix typeエラーを解消

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -1,7 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions);
 
   if (session) {
