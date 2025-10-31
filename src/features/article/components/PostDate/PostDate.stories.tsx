62104afbc8476c2783fbf06f6e3134c61872f355
Cloud <monaniza@gmail.com>
2025-10-31 14:45:54 +0900

    fix 命名変更

--- src/features/article/components/PostDate/PostDate.stories.tsx
+++ src/features/article/components/PostDate/PostDate.stories.tsx
@@ -34,3 +34,27 @@ export const Default: Story = {
     },
   },
 };
+
+export const update: Story = {
+  args: {
+    blog: {
+      id: "id1",
+      createdAt: "2025-09-15T10:00:00.000Z",
+      updatedAt: "2025-09-15T10:00:00.000Z",
+      publishedAt: "2025-09-15T10:00:00.000Z",
+      revisedAt: "2025-09-25T20:10:00.000Z",
+      categories: [
+        {
+          id: "id1",
+          name: "これはテストです",
+        },
+        {
+          id: "id2",
+          name: "これはテストです",
+        },
+      ],
+      title: "string",
+      body: "string",
+    },
+  },
+};
