55c7026af74ddeb36fe9a20b7fcfdfb0fe935735
Cloud <monaniza@gmail.com>
2025-11-07 16:30:16 +0900

    fix story の変更

--- src/features/Layout/Header/Header.stories.tsx
+++ src/features/Layout/Header/Header.stories.tsx
@@ -10,12 +10,12 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { children: null },
+  args: { session: null },
 };
 
 export const Login: Story = {
-  args: { children: null },
+  args: { session: null },
 };
 export const Logout: Story = {
-  args: { children: null },
+  args: { session: null },
 };
