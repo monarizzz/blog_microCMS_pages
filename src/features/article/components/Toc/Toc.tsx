9f16b03373b50015322c393112203cce31498d33
Cloud <monaniza@gmail.com>
2026-01-01 18:44:56 +0900

    feat ツリー構造の追加

--- src/features/article/components/Toc/Toc.tsx
+++ src/features/article/components/Toc/Toc.tsx
@@ -8,13 +8,19 @@ type Props = {
 };
 
 const Toc: NextPage<Props> = ({ toc }) => {
+  const tree = "└─  ";
   return (
     <div className={styles.tocRoot}>
       {toc.length != 0 ? (
         <ul className={styles.list}>
           {toc.map((data) => (
             <li key={data.id} className={styles.text}>
-              <Link href={`#${data.id}`}>{data.text}</Link>
+              <p className={styles[data.styles]}>
+                <Link href={`#${data.id}`}>
+                  {data.styles == "h1" ? null : tree}
+                  {data.text}
+                </Link>
+              </p>
             </li>
           ))}
         </ul>
