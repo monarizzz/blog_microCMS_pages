e109fef2fcb1d1ff87779e31b42e07f247f81795
Cloud <monaniza@gmail.com>
2026-01-14 19:12:19 +0900

    feat nextfontを使用したfontの適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Karla } from "next/font/google";
+
+const karla = Karla({
+  subsets: ["latin"],
+  weight: "300",
+  variable: "--font-karla",
+});
 
 export default function App({
   Component,
@@ -8,7 +15,9 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={karla.variable}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 }
