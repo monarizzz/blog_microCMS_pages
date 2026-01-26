8f469fafb74f68307663c7135582ade3f3231168
Cloud <monaniza@gmail.com>
2026-01-26 15:32:47 +0900

    ref: page/index.tsx -> app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,33 @@ type Props = {
   category: CategoryList;
 };
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
+// export const getStaticProps: GetStaticProps<Props> = async () => {
+//   const data = await getBlogList({ queries: { limit: 15 } });
+//   const categoryData = await getCategoriesList({ queries: { limit: 10 } });
+
+//   const blogsWithPlainText = data.contents.map((blog) => {
+//     const $ = cheerio.load(blog.body);
+//     // brタグを改行文字に置換
+//     $("br").replaceWith("\n");
+//     // ブロック要素の末尾に改行文字を追加
+//     $("h1, h2, h3, h4, h5, h6, p, li, blockquote").append("\n");
+//     const plainText = $.text().trim().slice(0, 150);
+//     return {
+//       ...blog,
+//       plainTextBody: plainText,
+//     };
+//   });
+
+//   return {
+//     props: {
+//       blogsWithPlainText: blogsWithPlainText,
+//       category: categoryData.contents,
+//     },
+//     revalidate: 86400,
+//   };
+// };
+
+const HomePage: NextPage<Props> = async () => {
   const data = await getBlogList({ queries: { limit: 15 } });
   const categoryData = await getCategoriesList({ queries: { limit: 10 } });
 
@@ -30,22 +56,15 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     };
   });
 
-  return {
-    props: {
-      blogsWithPlainText: blogsWithPlainText,
-      category: categoryData.contents,
-    },
-    revalidate: 86400,
-  };
-};
-
-const HomePage: NextPage<Props> = ({ blogsWithPlainText, category }) => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
 
   return (
-    <LayoutMain session={session}>
-      <HomeMain blogsWithPlainText={blogsWithPlainText} category={category} />
-    </LayoutMain>
+    // <LayoutMain session={session}>
+    <HomeMain
+      blogsWithPlainText={blogsWithPlainText}
+      category={categoryData.contents}
+    />
+    /* </LayoutMain> */
   );
 };
 
