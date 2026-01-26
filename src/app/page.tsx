a1742a37bd59982dab9ab2a7f37429ca67c2087b
Cloud <monaniza@gmail.com>
2026-01-26 23:26:47 +0900

    fix: コメントアウト削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,31 +14,7 @@ type Props = {
   category: CategoryList;
 };
 
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   const data = await getBlogList({ queries: { limit: 15 } });
-//   const categoryData = await getCategoriesList({ queries: { limit: 10 } });
-
-//   const blogsWithPlainText = data.contents.map((blog) => {
-//     const $ = cheerio.load(blog.body);
-//     // brタグを改行文字に置換
-//     $("br").replaceWith("\n");
-//     // ブロック要素の末尾に改行文字を追加
-//     $("h1, h2, h3, h4, h5, h6, p, li, blockquote").append("\n");
-//     const plainText = $.text().trim().slice(0, 150);
-//     return {
-//       ...blog,
-//       plainTextBody: plainText,
-//     };
-//   });
-
-//   return {
-//     props: {
-//       blogsWithPlainText: blogsWithPlainText,
-//       category: categoryData.contents,
-//     },
-//     revalidate: 86400,
-//   };
-// };
+// TODO:revalidateの設定
 
 const HomePage: NextPage<Props> = async () => {
   const data = await getBlogList({ queries: { limit: 15 } });
