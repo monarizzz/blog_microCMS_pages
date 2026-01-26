f83ba131315ce386191344bdebcf6ca99f948ab7
Cloud <monaniza@gmail.com>
2026-01-26 22:32:10 +0900

    fix: not-foundからsessionを削除

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,14 @@
 "use client";
 
 import LayoutMain from "@/commons/layout/components/LayoutMain/LayoutMain";
-import { useSession } from "next-auth/react";
 
-// TODO:session
 const NotFound = () => {
-  // const { data: session } = useSession();
-
   return (
-    // <LayoutMain session={session}>
-    <main className="main">
-      <p>ページがありません。</p>
-    </main>
-    // </LayoutMain>
+    <LayoutMain session={null}>
+      <main className="main">
+        <p>ページがありません。</p>
+      </main>
+    </LayoutMain>
   );
 };
 export default NotFound;
