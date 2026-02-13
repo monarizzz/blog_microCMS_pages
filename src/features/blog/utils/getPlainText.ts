760e41896ed19ab46ce398ee9bab858f365ecae9
Cloud <monaniza@gmail.com>
2026-02-13 19:19:21 +0900

    ref 置換を改行から空文字に変更

--- src/features/blog/utils/getPlainText.ts
+++ src/features/blog/utils/getPlainText.ts
@@ -2,12 +2,9 @@ import * as cheerio from "cheerio";
 
 const getPlainText = (htmlStrings: string) => {
   const $ = cheerio.load(htmlStrings);
-  $("br").replaceWith("\n");
-  $("h1, h2, h3, h4, h5, h6, p, li, blockquote").append("\n\n");
-  return $.text()
-    .replace(/\n{3,}/g, "\n\n")
-    .trim()
-    .slice(0, 150);
+  $("br").replaceWith(" ");
+  $("h1, h2, h3, h4, h5, h6, p, li, blockquote").append(" ");
+  return $.text().replace(/\s+/g, " ").trim().slice(0, 150);
 };
 
 export default getPlainText;
