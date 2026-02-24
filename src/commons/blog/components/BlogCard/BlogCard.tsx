f7e829c13d07b56d1700332a0d12d7a8bf7c5f20
Cloud <monaniza@gmail.com>
2026-02-24 20:54:22 +0900

    ref tailwindに

--- src/commons/blog/components/BlogCard/BlogCard.tsx
+++ src/commons/blog/components/BlogCard/BlogCard.tsx
@@ -10,19 +10,23 @@ type Props = {
 
 const BlogCard = ({ BlogWithPlainText }: Props) => {
   return (
-    <div className={styles.blogCardRoot}>
-      <div className={styles.tag}>
+    <div className="group rounded-2xl bg-white px-6 py-4 transition hover:-translate-y-0.5">
+      <div className="mb-2">
         {BlogWithPlainText.categories ? (
           <TagButton category={BlogWithPlainText.categories} maxLength={4} />
         ) : null}
       </div>
-      <div className={styles.content}>
+      <div className="ml-2 mt-3">
         <Link href={`/blog/${BlogWithPlainText.id}`}>
-          <p className={styles.title}>{BlogWithPlainText.title}</p>
-          <p className={styles.date}>
-            {relativeDate(BlogWithPlainText.publishedAt)}{" "}
+          <p className="mb-2 line-clamp-2 text-xl font-bold text-black group-hover:text-gray-700">
+            {BlogWithPlainText.title}
           </p>
-          <p className={styles.bodyText}>{BlogWithPlainText.plainTextBody}</p>
+          <p className="mb-2 block text-xs text-gray-400">
+            {relativeDate(BlogWithPlainText.publishedAt)}
+          </p>
+          <p className="lead mt-2 line-clamp-3 break-words text-sm text-gray-600">
+            {BlogWithPlainText.plainTextBody}
+          </p>
         </Link>
       </div>
     </div>
