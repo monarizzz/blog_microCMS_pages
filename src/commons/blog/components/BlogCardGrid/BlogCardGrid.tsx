8f354f686d7da677fb2a9f41ca942df7c2c0f205
Cloud <monaniza@gmail.com>
2026-02-24 23:50:26 +0900

    fix gap-5 gap-y-10 -> gap-x-5 gap-y-10

--- src/commons/blog/components/BlogCardGrid/BlogCardGrid.tsx
+++ src/commons/blog/components/BlogCardGrid/BlogCardGrid.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const BlogCardGrid = ({ blogWithPlainTextList }: Props) => {
   return (
-    <div className="grid grid-cols-1 gap-5 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-x-5 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {blogWithPlainTextList.map((BlogWithPlainText: BlogWithPlainText) => (
         <div key={BlogWithPlainText.id} className="w-full">
           <BlogCard BlogWithPlainText={BlogWithPlainText} />
