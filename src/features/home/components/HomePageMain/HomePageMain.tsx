2fc3871cdaba91135ba3a469eecda10aa664bb72
Cloud <monaniza@gmail.com>
2026-02-25 00:53:37 +0900

    ref tailwindに変更

--- src/features/home/components/HomePageMain/HomePageMain.tsx
+++ src/features/home/components/HomePageMain/HomePageMain.tsx
@@ -11,8 +11,8 @@ type Props = {
 
 const HomeMain = ({ blogsWithPlainText }: Props) => {
   return (
-    <div className={styles.homePageRoot}>
-      <p className={styles.title}>{UTILS_HOME.HOME}</p>
+    <div className="mx-auto my-6 w-4/5 max-w-7xl">
+      <p className="my-20 text-2xl font-bold">{UTILS_HOME.HOME}</p>
       <BlogCardGrid blogWithPlainTextList={blogsWithPlainText} />
     </div>
   );
