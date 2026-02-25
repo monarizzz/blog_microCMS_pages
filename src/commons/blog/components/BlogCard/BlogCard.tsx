a619d33f9f14da05f36f66c711e5df080dc5018e
Cloud <monaniza@gmail.com>
2026-02-25 16:46:23 +0900

    chore スタイルをconfigにまとめる

--- src/commons/blog/components/BlogCard/BlogCard.tsx
+++ src/commons/blog/components/BlogCard/BlogCard.tsx
@@ -20,10 +20,10 @@ const BlogCard = ({ BlogWithPlainText }: Props) => {
           <p className="mb-2 line-clamp-2 text-base font-bold text-black group-hover:text-gray-700">
             {BlogWithPlainText.title}
           </p>
-          <p className="mb-2 block text-[0.625rem] text-gray-400">
+          <p className="mb-2 block text-3xs text-gray-400">
             {relativeDate(BlogWithPlainText.publishedAt)}
           </p>
-          <p className="mt-2 line-clamp-2 break-words text-[0.6875rem] leading-5 tracking-wide text-gray-500 lg:line-clamp-3">
+          <p className="mt-2 line-clamp-2 break-words text-2xs leading-5 tracking-wide text-gray-500 lg:line-clamp-3">
             {BlogWithPlainText.plainTextBody}
           </p>
         </Link>
--- src/commons/tag/components/TagButtonText/TagButtonText.tsx
+++ src/commons/tag/components/TagButtonText/TagButtonText.tsx
@@ -11,7 +11,7 @@ type Props = {
 const TagButtonText = ({ name, maxLength, isSelected, categoryId }: Props) => {
   const text = `# ${limitText(name, maxLength)}`;
   const commonClass =
-    "mx-0.5 rounded-3xl border border-slate-500 px-3 py-1.5 text-[0.625rem] font-bold tracking-widest text-slate-500";
+    "mx-0.5 rounded-3xl border border-slate-500 px-3 py-1.5 text-3xs font-bold tracking-widest text-slate-500";
 
   if (isSelected) {
     return <span className={commonClass}>{text}</span>;
