abcce1b4e758496d00f5f198a5331c18630e37fc
Cloud <monaniza@gmail.com>
2026-02-25 17:14:00 +0900

    fix スタイルの変更,"BLOG"の削除

--- src/commons/layout/components/Header/Header.tsx
+++ src/commons/layout/components/Header/Header.tsx
@@ -7,31 +7,26 @@ type Props = {
 };
 
 const Header = ({ session }: Props) => {
+  const rightBtn =
+    "relative rounded-sm px-1.5 py-1 text-left text-sm font-bold text-gray-500 hover:bg-gray-100";
   return (
-    <div className="flex w-full justify-between p-3 shadow">
-      <Link href="/" className="my-auto ml-8 text-2xl font-bold text-gray-600">
-        {HEADER.TITLE}
+    <div className="flex w-full justify-between pb-1 pt-6 shadow">
+      <Link
+        href="/"
+        className="et my-auto ml-[3vw] text-2xl font-bold text-gray-600"
+      >
+        {/* {HEADER.TITLE} */}
       </Link>
-
-      <span className="mx-3 my-auto flex">
+      <span className="my-auto mr-[3vw] flex">
         {session ? (
-          <Link
-            href="/edit"
-            className="relative rounded-sm px-1.5 py-1 text-left text-base font-bold text-gray-500 hover:bg-gray-100"
-          >
+          <Link href="/edit" className={rightBtn}>
             {HEADER.EDIT}
           </Link>
         ) : null}
-        <Link
-          href="/"
-          className="relative rounded-sm px-1.5 py-1 text-left text-base font-bold text-gray-500 hover:bg-gray-100"
-        >
+        <Link href="/" className={rightBtn}>
           {HEADER.HOME}
         </Link>
-        <Link
-          href="/blog"
-          className="relative rounded-sm px-1.5 py-1 text-left text-base font-bold text-gray-500 hover:bg-gray-100"
-        >
+        <Link href="/blog" className={rightBtn}>
           {HEADER.CATEGORY}
         </Link>
         {/* <Link href="/about">
