7267fc70b5e2d8bf7323b603b504dc4dbad2d30c
Cloud <monaniza@gmail.com>
2026-02-26 13:24:47 +0900

    ref tailwindに変更

--- src/features/login/components/LoginPageMain/LoginPageMain.tsx
+++ src/features/login/components/LoginPageMain/LoginPageMain.tsx
@@ -1,26 +1,26 @@
 "use client";
 
 import { Session } from "@/infra/auth/google/session";
-import styles from "./LoginPageMain.module.scss";
 import { signIn, signOut } from "next-auth/react";
 
 type Props = {
   session: Session | null;
 };
 
+// TODO:スタイルを作成する
 const LoginPageMain = ({ session }: Props) => {
   return (
-    <div className={styles.loginPageMainRoot}>
+    <div className="flex h-svh items-center justify-center">
       {!session ? (
-        <div className={styles.signin}>
+        <div className="rounded-full bg-slate-200 p-3">
           <div>
-            <button className={styles.button} onClick={() => signIn("google")}>
+            <button className="" onClick={() => signIn("google")}>
               Googleでログイン
             </button>
           </div>
         </div>
       ) : (
-        <div className={styles.signOut}>
+        <div className="">
           <h1>{session.user && session.user.email}さん</h1>
           <button onClick={() => signOut()}>ログアウト</button>
         </div>
