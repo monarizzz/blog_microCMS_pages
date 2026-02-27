a18e44b523377298812cdd70ef4c894d871bcc5d
Cloud <monaniza@gmail.com>
2026-02-27 23:17:38 +0900

    fix

--- src/features/blog/components/Toc/Toc.tsx
+++ src/features/blog/components/Toc/Toc.tsx
@@ -7,13 +7,13 @@ type Props = {
 
 const Toc = ({ toc }: Props) => {
   return (
-    <div className="min-h-96 w-[20vw] rounded-lg bg-slate-100 p-4">
+    <div className="min-h-96 w-[20vw] rounded-lg bg-slate-50 p-4">
       {toc.length != 0 ? (
         <ul className="ml-1">
           {toc.map((data) => (
             <li
               key={data.id}
-              className="mb-0.5 line-clamp-2 overflow-hidden truncate break-words text-xs leading-9 text-slate-500 hover:text-indigo-600"
+              className="mb-0.5 line-clamp-2 truncate text-ellipsis break-words text-xs leading-9 text-slate-500 hover:bg-gray-100 hover:text-slate-500"
             >
               <Link href={`#${data.id}`}>{data.text}</Link>
             </li>
