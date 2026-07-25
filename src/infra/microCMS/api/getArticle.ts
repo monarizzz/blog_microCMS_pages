25edab2ef9267e89f68cac3aa4973a7403acb1f6
Ccc <monaniza@gmail.com>
2026-07-26 02:38:13 +0900

    feat Article APIの扱いを変更

--- src/infra/microCMS/api/getArticle.ts
+++ src/infra/microCMS/api/getArticle.ts
@@ -1,7 +1,7 @@
 import { client } from "@/infra/microCMS/client";
 import { MicroCMSListResponse, MicroCMSQueries } from "microcms-js-sdk";
+import { Article } from "../schema/article";
 
-// TODO:APIに破壊的変更をする必要があるので最後に実装
 export const getArticle = async ({
   q,
 }: MicroCMSQueries): Promise<MicroCMSListResponse<Article>> => {
