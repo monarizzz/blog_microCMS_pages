8c931d97d89cbc23a3273f2bbd2cf3b47c2eecf3
Ccc <monaniza@gmail.com>
2026-07-26 02:09:15 +0900

    feat 型とコメントの追加

--- src/infra/microCMS/api/getArticle.ts
+++ src/infra/microCMS/api/getArticle.ts
@@ -1,6 +1,8 @@
 import { client } from "@/infra/microCMS/client";
+import { MicroCMSListResponse } from "microcms-js-sdk";
 
-export const getArticle = async () => {
+// TODO:APIに破壊的変更をする必要があるので最後に実装
+export const getArticle = async (): Promise<MicroCMSListResponse<Article>> => {
   return await client.getList({
     endpoint: "article",
   });
