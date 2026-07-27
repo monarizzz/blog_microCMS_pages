0278b745fd2c36e2bf73dae32a0689fe5d40e39e
Ccc <monaniza@gmail.com>
2026-07-27 18:04:40 +0900

    fix Tags types

--- src/infra/microCMS/schema/profile.ts
+++ src/infra/microCMS/schema/profile.ts
@@ -1,7 +1,8 @@
+import { MicroCMSListContent } from "microcms-js-sdk";
 import { Tags } from "./tags";
 
 export type Profile = {
   name: string;
   bio: string;
-  tags: Tags[];
+  tags: (Tags & MicroCMSListContent)[];
 };
