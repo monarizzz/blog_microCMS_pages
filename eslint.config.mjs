62e4066c7f469ebd977f00ac30369f0ef60fb030
Ccc <monaniza@gmail.com>
2026-07-28 16:11:05 +0900

    chore Tailwind Update to v4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
-import tailwind from "eslint-plugin-tailwindcss";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -16,7 +15,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...storybook.configs["flat/recommended"],
-  ...tailwind.configs["flat/recommended"],
 ];
 
 export default eslintConfig;
