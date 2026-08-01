12b931d8d85f3ae72c3f4ff1fd95a1ed9e2b5378
Cloud <monaniza@gmail.com>
2026-08-01 23:17:51 +0900

    ref 重複しているTagの描画をまとめる

--- src/commons/article/components/ArticleRow/ArticleRow.tsx
+++ src/commons/article/components/ArticleRow/ArticleRow.tsx
@@ -3,13 +3,21 @@ import TagBtn from "@/commons/layout/components/TagBtn/TagBtn";
 const title = "Next.js 14 App Router 移行の勘所";
 const expect =
   "PagesRouterからの移行を検討しているプロジェクトも多いことでしょう。最大のパラダイムシフトは、ReactServerComponentsをデフォルトとする設計思想にあります。";
-const tagNum = ["タグ", "Next.js"];
+const tagList = ["タグ", "Next.js"];
 
 type Props = {
   compact?: boolean;
 };
 
 const ArticleRow = ({ compact }: Props) => {
+  const tags = (
+    <div className="flex gap-2">
+      {tagList.map((tag) => (
+        <TagBtn key={tag} text={tag} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="border-outline-variant flex w-full gap-8 border-b py-6">
       <p className="text-secondary my-auto text-sm tracking-[0.5px]">
@@ -20,21 +28,13 @@ const ArticleRow = ({ compact }: Props) => {
           <p className="text-primary text-[15.5px] font-bold tracking-[0.2px]">
             {title}
           </p>
-          <div className="flex gap-2">
-            {tagNum.map((tag) => (
-              <TagBtn key={tag} text={tag} />
-            ))}
-          </div>
+          <div className="flex gap-2">{tags}</div>
         </div>
       ) : (
         <div className="flex flex-col gap-2">
           <p className="text-primary font-bold">{title}</p>
           <p className="text-on-surface-variant">{expect}</p>
-          <div className="flex gap-2">
-            {tagNum.map((tag) => (
-              <TagBtn key={tag} text={tag} />
-            ))}
-          </div>
+          <div className="flex gap-2">{tags}</div>
         </div>
       )}
     </div>
