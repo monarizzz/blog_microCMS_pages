a0c459079a46b0a05232ebc0ce4b4164c8233cc5
Cloud <monaniza@gmail.com>
2026-08-01 13:55:36 +0900

    feat footer追加

---
+++ src/commons/layout/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import GlobalNav from "../GlobalNav/GlobalNav";
+
+type Props = {
+  pageList: string[];
+};
+
+const Footer = ({ pageList }: Props) => {
+  return (
+    <div className="bg-surface h-full px-8 py-24">
+      <div className="flex flex-col gap-4">
+        <GlobalNav pageList={pageList} />
+        <span className="-spacing-[0.5px] text-[24px]">MoneLogue</span>
+        <span className="text-sm">© 2024 MoneLogue</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
