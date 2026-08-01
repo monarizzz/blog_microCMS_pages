e9d1287d52612eb2bd1f24818a43f559d1130315
Cloud <monaniza@gmail.com>
2026-08-01 23:11:05 +0900

    fix タグを表示し忘れ

--- src/commons/article/components/ArticleRow/ArticleRow.tsx
+++ src/commons/article/components/ArticleRow/ArticleRow.tsx
@@ -14,9 +14,16 @@ const ArticleRow = ({ compact }: Props) => {
     <div className="border-outline-variant flex w-full gap-8 border-b py-6">
       <p className="space-[0.5px] text-secondary my-auto text-sm">2024.03.18</p>
       {compact ? (
-        <p className="text-primary space-[0.2px] text-[15.5px] font-bold">
-          {title}
-        </p>
+        <div className="flex w-full justify-between">
+          <p className="text-primary space-[0.2px] text-[15.5px] font-bold">
+            {title}
+          </p>
+          <div className="flex gap-2">
+            {tagNum.map((tag) => (
+              <TagBtn text={tag} />
+            ))}
+          </div>
+        </div>
       ) : (
         <div className="flex flex-col gap-2">
           <p className="text-primary font-bold">{title}</p>
